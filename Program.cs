f02a639c1694f846997e19cfb65ddbe77ee37609
André Bachman <andre.bachman@brightstraining.com>
2024-10-24 10:42:22 +0200

    Removed text from Program.cs

--- Program.cs
+++ Program.cs
@@ -1,13 +1,12 @@
 ﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
 
 Console.WriteLine("Hello, World!");
-<<<<<<< HEAD
+
 Console.WriteLine("I live in Denmark");
 Console.WriteLine("I like pandas");
 
 System.Console.WriteLine("Welcome Bache13");
 System.Console.WriteLine("Another welcome");
-=======
 
 string text = System.IO.File.ReadAllText("panda-info.txt");
 System.Console.WriteLine($"Here in info from the file 👉 {text}");
