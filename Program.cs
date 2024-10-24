048dc670402f9180694792add2c82250094737cb
André Bachman <andre.bachman@brightstraining.com>
2024-10-24 10:27:04 +0200

    Added Welcome Bache in Program.cs

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ Console.WriteLine("Hello, World!");
 <<<<<<< HEAD
 Console.WriteLine("I live in Denmark");
 Console.WriteLine("I like pandas");
+
+System.Console.WriteLine("Welcome Bache13");
 =======
 
 string text = System.IO.File.ReadAllText("panda-info.txt");
