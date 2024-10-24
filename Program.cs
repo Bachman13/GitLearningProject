3a30335c693497d6c4001ea702f0a32517ecebd1
André Bachman <andre.bachman@brightstraining.com>
2024-10-24 10:50:17 +0200

    Removed stuff

--- Program.cs
+++ Program.cs
@@ -5,8 +5,7 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("I live in Denmark");
 Console.WriteLine("I like pandas");
 
-System.Console.WriteLine("Welcome Bache13");
-System.Console.WriteLine("Another welcome");
+
 
 string text = System.IO.File.ReadAllText("panda-info.txt");
 System.Console.WriteLine($"Here in info from the file 👉 {text}");
